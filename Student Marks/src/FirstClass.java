e91690a6e91920ce3d9a7766b1cc381eb72bffba
ysnay <ysnay@DESKTOP-VK6EO2U>
2020-06-26 15:17:43 +0530

    First_Commit

---
+++ Student Marks/src/FirstClass.java
@@ -0,0 +1,18 @@
+
+public class FirstClass
+{
+	public static void main(String args[])
+	{
+        int num=2;
+		switch (num)
+		{
+		    case 1:System.out.println("One");
+		           break;
+		    case 2:System.out.println("Two");
+		           break;
+		    default:System.out.println("Invalid");
+		}
+
+	}
+
+}
